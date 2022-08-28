73626752a89aa9a715a6f017bfd9cd59b0a95322
Phuc <pvo@Phucs-MBP.lan>
2022-08-28 11:59:31 +0700

    custom dashboard admin

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,4 +1,23 @@
 from django.contrib import admin
 from .models import User
+from django.contrib.auth.admin import UserAdmin
 # Register your models here.
-admin.site.register(User)
+
+
+class CustomUserAdmin(UserAdmin):
+    list_display = ('email', 'fullname', 'username',
+                    'is_admin', 'role', 'is_active')
+    ordering = ('is_admin', 'created_date')
+    list_filter = ()
+
+    fieldsets = (
+
+    )
+
+    search_fields = ()
+    ordering = ()
+
+    filter_horizontal = ()
+
+
+admin.site.register(User, CustomUserAdmin)
