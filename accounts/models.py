7d90308c58ed773441fa909eb5a45c63f2fd2dfb
Phuc <pvo@Phucs-MBP.lan>
2022-08-28 12:11:10 +0700

    add user profile and setting media

--- accounts/models.py
+++ accounts/models.py
@@ -1,5 +1,6 @@
 from multiprocessing.sharedctypes import Value
 from operator import mod
+from pickle import TRUE
 from django.db import models
 from django.contrib.auth.models import AbstractBaseUser, BaseUserManager
 
@@ -78,3 +79,18 @@ class User(AbstractBaseUser):
 
     def has_module_perms(self, app_label):
         return True
+
+
+class UserProfile(models.Model):
+    user = models.OneToOneField(
+        User, on_delete=models.CASCADE, blank=True, null=True)
+    profile_picture = models.ImageField(
+        upload_to='users/profile_pictures', blank=True, null=True)
+    cover_photo = models.ImageField(
+        upload_to='users/cover_photos', blank=True, null=True)
+    address = models.CharField(max_length=225, blank=True, null=TRUE)
+    created_date = models.DateTimeField(auto_now_add=True)
+    updated_date = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.user.email
--- main/settings.py
+++ main/settings.py
@@ -127,6 +127,11 @@ STATIC_ROOT = BASE_DIR/'static'
 STATICFILES_DIRS = [
     'main/static'
 ]
+
+# Media staic
+
+MEDIA_URL = 'media/'
+MEDIA_ROOT = BASE_DIR/'media'
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
